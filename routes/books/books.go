35ee2f80beada05ad400a422b4024ca9ee488c1b
Dudu Neto <dudumatosneto@gmail.com>
2023-07-18 22:53:42 -0300

    feat: add new endpoint

--- routes/books/books.go
+++ routes/books/books.go
@@ -1,37 +1,111 @@
 package books_routes
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 )
 
+type Book struct {
+	Id        uuid.UUID
+	CreatedAt time.Time
+	Title     string
+	Genre     string
+}
+
+type ListBookResponse struct {
+	Total int
+	Data  []Book
+}
+
+var listOfBooks []Book = []Book{}
+
 func books(writeResponse http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	bookId, _ := vars["id"]
+	fmt.Println(bookId)
+
 	switch request.Method {
 	case "POST":
 		registerBooks(writeResponse, request)
+	case "GET":
+		listBooks(writeResponse, request)
 	default:
 		fmt.Fprint(writeResponse, "ROUTE NOT FOUND")
 	}
 }
 
-func registerBooks(writeResponse http.ResponseWriter, request *http.Request) {
-	body, err_read_body := ioutil.ReadAll(request.Body)
-	if err_read_body != nil {
-		http.Error(writeResponse, "Failed to read request body", http.StatusInternalServerError)
+func registerBooks(w http.ResponseWriter, r *http.Request) {
+	var newBook Book
+	err := json.NewDecoder(r.Body).Decode(&newBook)
+	newBook.Id = uuid.New()
+	newBook.CreatedAt = time.Now()
+	if err != nil {
+		http.Error(w, "Failed to decode request body", http.StatusBadRequest)
 		return
 	}
 	// Close the request body to prevent resource leaks
-	defer request.Body.Close()
-	_, err := writeResponse.Write(body)
+	defer r.Body.Close()
+
+	// Append the new book to the listOfBooks
+	listOfBooks = append(listOfBooks, newBook)
+
+	// Send a response
+	w.WriteHeader(http.StatusCreated)
+}
+
+func listBooks(writeResponse http.ResponseWriter, request *http.Request) {
+	bufResponse, _errConvertToByte := json.Marshal(
+		ListBookResponse{
+			Total: len(listOfBooks),
+			Data:  listOfBooks,
+		},
+	)
+	if _errConvertToByte != nil {
+		http.Error(writeResponse, "Failed to convert into JSON", http.StatusInternalServerError)
+		return
+	}
+	writeResponse.Header().Set("Content-Type", "application/json")
+	_, err := writeResponse.Write(bufResponse)
 	if err != nil {
 		http.Error(writeResponse, "Failed to write response body", http.StatusInternalServerError)
 		return
 	}
 }
 
-func listBooks() {
+func getSingle(writeResponse http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	bookId, err := vars["id"]
+	fmt.Println(bookId)
+	if err != false {
+		http.Error(writeResponse, "Pass a valid id", http.StatusInternalServerError)
+		return
+	}
+	var found = Book{}
+	for _, book := range listOfBooks {
+		if book.Id.String() == bookId {
+			found = book
+		}
+	}
+	generateResponseRequest(found, writeResponse, request)
+}
 
+func generateResponseRequest(payload any, writeResponse http.ResponseWriter, request *http.Request) {
+	bufResponse, _errConvertToByte := json.Marshal(payload)
+	if _errConvertToByte != nil {
+		http.Error(writeResponse, "Failed to convert into JSON", http.StatusInternalServerError)
+		return
+	}
+	writeResponse.Header().Set("Content-Type", "application/json")
+	_, err := writeResponse.Write(bufResponse)
+	if err != nil {
+		http.Error(writeResponse, "Failed to write response body", http.StatusInternalServerError)
+		return
+	}
 }
 
 func HandleBooksRoutes(server *http.ServeMux) {
